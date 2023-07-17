159305ea1bb648c1fb294701102a9ff7780686e7
Ruben Romero <romeroa.ruben@gmail.com>
2023-07-17 13:49:40 -0300

    Commit Inicial

---
+++ src/models/libroModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/biblioteca", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const LibroSchema = new mongoose.Schema({
+  titulo: String,
+  autor: String
+}, { collection: 'libros' });
+
+const Libro = mongoose.model('Libro', LibroSchema);
+
+module.exports = Libro;
