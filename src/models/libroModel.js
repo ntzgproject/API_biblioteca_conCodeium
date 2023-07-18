f68ae3e08add5654f40d6e3769954d25bc566c99
Ruben Romero <romeroa.ruben@gmail.com>
2023-07-18 17:22:32 -0300

    update mongodb URL

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/biblioteca", {
+mongoose.connect(process.env.MONGO_DB, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
